16fab793fed0710244e052cd663b388929bcba41
soltanoff <piccadillable@gmail.com>
2017-08-30 19:30:24 +0300

    added function timer, created the sorting function list

--- main.cpp
+++ main.cpp
@@ -1,6 +1,41 @@
-#include <iostream>
+#include <ctime>
+#include <iomanip>
+#include <ratio>
+#include <chrono>
+#include "settings.h"
 
+
+FuncVector algorithms{
+        sortByBubble,
+        sortByCounting,
+        sortByExtraction,
+        sortByHeap,
+        sortByInclusion,
+        sortByMerge,
+        sortByQuick
+};
+
+
+// TODO: need to test all sorting functions
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    Array array{1, 2, 6, 8, 99, 1, 4, 6, 7};
+
+    for (auto f: algorithms) {
+        std::clock_t c_start = std::clock();
+        auto t_start = std::chrono::high_resolution_clock::now();
+
+        f(array);
+
+        auto t_end = std::chrono::high_resolution_clock::now();
+        std::clock_t c_end = std::clock();
+
+        std::cout << std::fixed << std::setprecision(2) << "CPU time used: "
+                  << 1000.0 * (c_end - c_start) / CLOCKS_PER_SEC << " ms\n"
+                  << "Wall clock time passed: "
+                  << std::chrono::duration<double, std::milli>(t_end - t_start).count()
+                  << " ms\n";
+    }
+
+    std::cout << std::endl;
     return 0;
 }
