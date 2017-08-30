bad1826b1706aec26969fc62fdced08c692f86e7
soltanoff <piccadillable@gmail.com>
2017-08-30 19:51:34 +0300

    added the TODO and the FIXME marks

--- main.cpp
+++ main.cpp
@@ -17,10 +17,12 @@ FuncVector algorithms{
 
 
 // TODO: need to test all sorting functions
+// TODO: change a timer logic
 int main() {
     Array array{1, 2, 6, 8, 99, 1, 4, 6, 7};
 
     for (auto f: algorithms) {
+        // FIXME: wtf?! Change this std::clock
         std::clock_t c_start = std::clock();
         auto t_start = std::chrono::high_resolution_clock::now();
 
@@ -33,9 +35,8 @@ int main() {
                   << 1000.0 * (c_end - c_start) / CLOCKS_PER_SEC << " ms\n"
                   << "Wall clock time passed: "
                   << std::chrono::duration<double, std::milli>(t_end - t_start).count()
-                  << " ms\n";
+                  << " ms\n\n";
     }
 
-    std::cout << std::endl;
     return 0;
 }
